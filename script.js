11768f8cab19b6968973f299040d266ffd464fdb
ZF5657 <zane56fields57@gmail.com>
2024-01-31 16:26:46 -0500

    Add read property to book class, radio function for array, and toggle read button

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ const authorInput = document.querySelector('#authorInput');
 const pagesInput = document.querySelector('#pagesInput');
 const libraryDisplay = document.querySelector('.libraryDisplay');
 const plusBtn = document.querySelector('.plus');
+const readRadio = document.querySelector('#read');
 
 const myLibrary = [];
 
 class Book{
-    constructor  (title, author, pages) {
+    constructor  (title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
+        this.read = read;
     }
-}
+};
 
 const addBookToLibrary = () => {
     let title = titleInput.value;
     let author = authorInput.value;
     let pages = pagesInput.value;
-    const newBook =  new Book(title, author, pages);
+    let read = '';
+    if(readRadio.checked == true) {
+        read = true
+    } else {
+        read = false
+    };
+    const newBook =  new Book(title, author, pages, read);
     myLibrary.push(newBook);
 
     //Loop to add new divs for each new book
@@ -37,13 +45,37 @@ const addBookToLibrary = () => {
             bookDiv.appendChild(bookTitleDiv);
             bookDiv.appendChild(bookAuthorDiv);
             bookDiv.appendChild(bookPagesDiv);
+            bookTitleDiv.textContent = 'Title: ' + book.title;
+            bookAuthorDiv.textContent = 'Author: ' + book.author;
+            bookPagesDiv.textContent = 'Pages: ' + book.pages;
+            let readBtn = document.createElement("button");
+            readBtn.classList.add('readBtn');
+            let toggleRead = '';
+            if(readRadio.checked == true) {
+                readBtn.textContent = 'Read'
+                toggleRead = true
+            } else {
+                readBtn.textContent = 'Not Read'
+                toggleRead = false
+            };
+            readBtn.addEventListener('click', () => {
+                if(toggleRead == true) {
+                    readBtn.textContent = 'Not Read'
+                    toggleRead = false
+                    book.read = false
+                    console.log(myLibrary)
+                } else {
+                    readBtn.textContent = 'Read'
+                    toggleRead = true
+                    book.read = true
+                    console.log(myLibrary)
+                }
+            });
+            bookDiv.appendChild(readBtn);
             const trash = document.createElement("img");
             trash.src = "svg/trashcan.svg";
             trash.classList.add('trash');
             bookDiv.appendChild(trash);
-            bookTitleDiv.textContent = 'Title: ' + book.title;
-            bookAuthorDiv.textContent = 'Author: ' + book.author;
-            bookPagesDiv.textContent = 'Pages: ' + book.pages;
             
             //function to delete book from array and div
             trash.addEventListener('click', () => {
@@ -56,11 +88,11 @@ const addBookToLibrary = () => {
             })
         }
     })
-}
+};
 
 plusBtn.addEventListener('click', () => {
-    document.querySelector('.userInput').style = ('visibility: visible;')
-})
+    document.querySelector('.userInput').style = ('display: flex;')
+});
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -72,5 +104,6 @@ submitBtn.addEventListener('click', (e) => {
     authorInput.textContent = '';
     pagesInput.value = '';
     pagesInput.textContent = '';
-})
+    document.querySelector('.userInput').style = ('display: none;');
+});
 
